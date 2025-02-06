002c177f5ab51d1beb2e8af91ff3a319d9252d6f
valkuros@gmail.com <valkuros@gmail.com>
2025-02-05 23:40:44 -0500

    Added logic to finish order process. added keypad to select quantities

--- src/main/java/org/mike/userinterface/GraphicalUI.java
+++ src/main/java/org/mike/userinterface/GraphicalUI.java
@@ -3,21 +3,29 @@ package org.mike.userinterface;
 import org.mike.service.OrderService;
 
 import javax.swing.*;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Random;
 
-public class GraphicalUI {
+public class GraphicalUI extends JPanel implements ActionListener {
 private OrderMenu orderMenu;
 private JFrame frame;
 private JPanel cardPanel;
 private CardLayout cardLayout;
 private JPanel mainMenuPanel;
 private JPanel orderPanel;
+private Document display = null;
+private int orderQtyLemonade1;
+private int orderQtyLemonade2;
 
 public GraphicalUI(OrderService orderService) {
+
 	frame = new JFrame("Lemonade Administration");
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.setSize(300, 300);
+	frame.setSize(640, 960);
 
 	var webIcon = new ImageIcon("src/main/resources/pictures/lemonIcon.png");
 	frame.setIconImage(webIcon.getImage());
@@ -32,6 +40,20 @@ public GraphicalUI(OrderService orderService) {
 
 	frame.add(cardPanel);
 	frame.setVisible(true);
+}
+
+@Override
+public void actionPerformed(ActionEvent event) {
+		String key = event.getActionCommand();
+	try {
+		display.insertString(display.getLength(), key, null);
+	} catch (BadLocationException e) {
+		e.printStackTrace();
+	}
+}
+
+static class LemonadeWrapper {
+	int lemonadeID;
 }
 
 private void createMainMenu() {
@@ -71,6 +93,10 @@ private void createMainMenu() {
 
 
 private void createOrderPanel() {
+
+	final LemonadeWrapper wrapper = new LemonadeWrapper();
+	wrapper.lemonadeID = 0;
+
 	orderPanel = new JPanel();
 	orderPanel.setLayout(new BorderLayout());
 
@@ -87,55 +113,106 @@ private void createOrderPanel() {
 	topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.Y_AXIS));
 	topPanel.setBackground(Color.white);
 
-
 	topPanel.add(orderIdPanel);
+	topPanel.add(Box.createVerticalGlue());
 
-	topPanel.add(Box.createRigidArea(new Dimension(0, 20)));
 
-
-	JButton button3 = new JButton("Healthy Lemonade");
+	JButton button3 = new JButton("     Healthy Lemonade $12.00");
 	button3.setBackground(Color.cyan);
 	button3.setFont(new Font("Tahoma", Font.BOLD, 14));
-	button3.setIcon(new ImageIcon(new ImageIcon("src/main/resources/pictures/waterIcon.png").getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH)));
+	button3.setIcon(new ImageIcon(new ImageIcon("src/main/resources/pictures/waterIcon.png").getImage().getScaledInstance(20, 50, Image.SCALE_SMOOTH)));
+	button3.addActionListener(e -> {
+		wrapper.lemonadeID = 1;
+		orderPanel.revalidate();
+		orderPanel.repaint();
+		qtyPad(1);
+	});
 
-	JButton button4 = new JButton("Original Lemonade");
+	JButton button4 = new JButton("     Original Lemonade  $10.00");
 	button4.setBackground(Color.green);
 	button4.setFont(new Font("Tahoma", Font.BOLD, 14));
-	button4.setIcon(new ImageIcon(new ImageIcon("src/main/resources/pictures/lemonadeIcon.png").getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH)));
+	button4.setIcon(new ImageIcon(new ImageIcon("src/main/resources/pictures/lemonadeIcon.png").getImage().getScaledInstance(50, 80, Image.SCALE_SMOOTH)));
+	button4.addActionListener(e -> {
+		wrapper.lemonadeID = 2;
+		orderPanel.revalidate();
+		orderPanel.repaint();
+		qtyPad(2);
+	});
 
-	Dimension buttonSize = new Dimension(230, 80);
+	Dimension buttonSize = new Dimension(400, 100);
 	button3.setPreferredSize(buttonSize);
 	button4.setPreferredSize(buttonSize);
 
 
-	JPanel buttonPanel3 = new JPanel();
-	buttonPanel3.setLayout(new FlowLayout(FlowLayout.CENTER));
-	buttonPanel3.setBackground(Color.white);
-	buttonPanel3.add(button3);
-	topPanel.add(buttonPanel3);
+	JPanel buttonPanel = new JPanel();
+	buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+	buttonPanel.setBackground(Color.white);
+	buttonPanel.add(button3);
+	buttonPanel.add(Box.createRigidArea(new Dimension(0, 20)));
+	buttonPanel.add(button4);
 
-	topPanel.add(Box.createRigidArea(new Dimension(0, 5)));
+	topPanel.add(buttonPanel);
+	topPanel.add(Box.createVerticalGlue());
 
-	JPanel buttonPanel4 = new JPanel();
-	buttonPanel4.setLayout(new FlowLayout(FlowLayout.CENTER));
-	buttonPanel4.setBackground(Color.white);
-	buttonPanel4.add(button4);
-	topPanel.add(buttonPanel4);
 
-	orderPanel.add(topPanel, BorderLayout.NORTH);
+	orderPanel.add(topPanel, BorderLayout.CENTER);
 
 
 	JPanel buttonPanelBottom = new JPanel(new FlowLayout(FlowLayout.LEFT));
+
+	JButton button5 = new JButton("CHECK OUT");
+	button5.setBackground(Color.yellow);
+	button5.setFont(new Font("Tahoma", Font.BOLD, 10));
+	button5.addActionListener(e -> cardLayout.show(cardPanel, "MainMenu"));
+	buttonPanelBottom.add(button5);
+
+
 	JButton button2 = new JButton("Cancel");
 	button2.setBackground(Color.red);
 	button2.setFont(new Font("Tahoma", Font.BOLD, 10));
 	button2.addActionListener(e -> cardLayout.show(cardPanel, "MainMenu"));
-
 	buttonPanelBottom.add(button2);
+	buttonPanelBottom.add(Box.createRigidArea(new Dimension(0, 50)));
 	orderPanel.add(buttonPanelBottom, BorderLayout.SOUTH);
 
 
 	cardPanel.add(orderPanel, "OrderPanel");
 }
+
+
+public void qtyPad(int lemonadetype) {
+
+		JFrame frame = new JFrame("How many would you like?");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		frame.setLayout(new BorderLayout());
+
+		if (lemonadetype == 1) {
+			JPanel buttons = new JPanel(new GridLayout(3, 3));  // 3x3 grid
+			for (int i = 1; i < 10; i++) {
+				JButton button = new JButton(Integer.toString(i));
+				button.addActionListener(e -> {
+					this.orderQtyLemonade1 += Integer.parseInt(button.getText());
+				});
+				buttons.add(button);
+			}
+
+		} else if (lemonadetype == 2) {
+			JPanel buttons = new JPanel(new GridLayout(3, 3));  // 3x3 grid
+			for (int i = 1; i < 10; i++) {
+				JButton button = new JButton(Integer.toString(i));
+				button.addActionListener(e -> {
+					this.orderQtyLemonade2 += Integer.parseInt(button.getText());
+				});
+				buttons.add(button);
+		}
+
+		frame.add(buttons, BorderLayout.CENTER);
+		frame.pack();
+	}
+}
+
+
+
 
 }
